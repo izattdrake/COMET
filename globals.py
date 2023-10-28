da1ee0886ff4930b75b60346affd69545e5f2c27
izattdrake <izattdrake@gmail.com>
2023-10-28 18:03:27 -0500

    Updated globals

--- globals.py
+++ globals.py
@@ -4,24 +4,30 @@ Created on Fri Oct 27 22:10:32 2023
 @author: izattdrake
 """
 
+H = 'Hydrogen'
 AR = 'Argon'
 N = 'Nitrogen'
 
-AMU_AR = 39.948
-AMU_N = 14.007
+PI = 3.141592653589
 
+CHARGE_E_SI = 1.602 * 10**(-19)
+KB_SI = 1.381 * 10**(-23)
+KB_EV = 8.617 * 10**(-5)
+EPSILON_SI = 8.85418782 * 10**(-12)
+MU_SI = 4 * PI * 10**(-7)
+C_SI = 2.99792458 * 10**8
+
+MASS_H_AMU = 1.00784
+MASS_AR_AMU = 39.948
+MASS_N_AMU = 14.007
 MASS_E_SI = 9.109 * 10**(-31)
 MASS_P_SI = 1.673 * 10**(-27)
 MASS_N_SI = 1.675 * 10**(-27)
-MASS_E_EV = 0.511 * 10**6
-MASS_P_EV = 938.272 * 10**6 
-MASS_N_EV = 939.565 * 10**6
+MASS_E_EV = 0.511 * 10**6 / C_SI**2
+MASS_P_EV = 938.272 * 10**6 / C_SI**2
+MASS_N_EV = 939.565 * 10**6 / C_SI**2
 
-CHARGE_E_SI = 1.602 * 10**(-19)
 
-KB_SI = 1.381 * 10**(-23)
-KB_EV = 8.617 * 10**(-5)
-
+KG_TO_EV = 5.6095886 * 10**35 / C_SI**2
+AMU_TO_KG = 1.67377 * 10**(-27)
 MTORR_TO_PA = 0.133
-
-PI = 3.1415926535
